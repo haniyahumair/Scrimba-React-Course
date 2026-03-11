daf33d5586fe168ee0463e7ec15f8b8b939aafe7
Haniyah Umair <haniyah.umair.18@gmail.com>
2026-03-11 16:05:09 +0300

    Completed meme generator

---
+++ meme-generator/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import trollFace from "../assets/Trollface.png"
+
+export default function Header() {
+    return (
+        <header className="header">
+            <img 
+                src={trollFace} 
+            />
+            <h1>Meme Generator</h1>
+        </header>
+    )
+}
